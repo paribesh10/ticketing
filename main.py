7efd1eb58424a1d5f12ee4d13c48415c146d73ed
paribesh10 <62760565+paribesh10@users.noreply.github.com>
2022-03-16 17:55:26 +0000

    Added about the photos

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height(cm)? "))
+bill = 0
 
 if height >= 120:
+  print("You can ride the rollercoaster!")
   age =int(input("What is your age? "))
   if age < 12:
-    print("Please pay $5!")
+    bill = 5
+    print("Child ticket are $5!")
   elif age <= 18:
-    print("Please pay $7!")
+    bill = 7
+    print("Youth tickets are $7!")
   else:
-    print("Please pay $12!")
+    bill = 12
+    print("Adult tickets are $12!")
+
+  wants_photos = input("Do you want a photo taken? Y or N. ")
+  if wants_photos == "Y":
+    #Adding $3 to bill
+    bill += 3
+
+  print(f"Your final bill is ${bill}")
+  
 else:
   print("Sorry! you can not ride!")
